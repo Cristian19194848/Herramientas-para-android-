86b327c275c86df127aff6b37b0a71e770d0adb2
Cristian19194848 <95430377+Cristian19194848@users.noreply.github.com>
2022-04-19 11:06:37 -0300

    cambio de desinstalar a deshabilitar
    Este script deshabilita la mayoria de apps de g00gle

--- ADB/Desg00glear android/Desg00glear.sh
+++ ADB/Desg00glear android/Desg00glear.sh
@@ -12,124 +12,124 @@ echo "ingrese el numero de usuario que quiere modificar: " usuario
 read usuario 
 echo "bien tu usuario es $usuario"
 sleep 2
- pm uninstall --user $usuario com.google.android.gsf  
- echo "desinstalando com.google.android.gsf"
+ pm disable-user --user $usuario com.google.android.gsf  
+ echo "Deshabilitando com.google.android.gsf"
  
- pm uninstall --user $usuario com.google.android.gsm  
-echo " desinstalando com.google.android.gsm"
+ pm disable-user --user $usuario com.google.android.gsm  
+echo " Deshabilitando com.google.android.gsm"
 
-pm uninstall --user $usuario com.google.android.tts  
- echo "desinstalandocom.google.android.tts"
+pm disable-user --user $usuario com.google.android.tts  
+ echo "Deshabilitandocom.google.android.tts"
  
- pm uninstall --user $usuario com.google.android.apps.googleassistant  
-echo "desinstalando com.google.android.apps.googleassistant"
+ pm disable-user --user $usuario com.google.android.apps.googleassistant  
+echo "Deshabilitando com.google.android.apps.googleassistant"
 
-pm uninstall --user $usuario com.google.android.googlequicksearchbox  
-echo "desinstalando com.google.android.googlequicksearchbox"
+pm disable-user --user $usuario com.google.android.googlequicksearchbox  
+echo "Deshabilitando com.google.android.googlequicksearchbox"
 
- pm uninstall --user $usuario com.android.vending  
- echo "desinstalando  com.android.vending"
+ pm disable-user --user $usuario com.android.vending  
+ echo "Deshabilitando  com.android.vending"
  
- pm uninstall --user $usuario com.google.android.videos  
- echo "desinstalando com.google.android.videos"
+ pm disable-user --user $usuario com.google.android.videos  
+ echo "Deshabilitando com.google.android.videos"
  
- pm uninstall --user $usuario com.google.android.gsf  
-echo "desinstalando com.google.android.gsf"
+ pm disable-user --user $usuario com.google.android.gsf  
+echo "Deshabilitando com.google.android.gsf"
 
-pm uninstall --user $usuario com.google.android.gms
-echo "desinstalando com.google.android.gms"
+pm disable-user --user $usuario com.google.android.gms
+echo "Deshabilitando com.google.android.gms"
  
-pm uninstall --user $usuario com.google.android.backuptransport  
- echo "desinstalando com.google.android.backuptransport"
+pm disable-user --user $usuario com.google.android.backuptransport  
+ echo "Deshabilitando com.google.android.backuptransport"
  
-pm uninstall --user $usuario com.google.android.ext.services  
- echo "desinstalando com.google.android.ext.services"
+pm disable-user --user $usuario com.google.android.ext.services  
+ echo "Deshabilitando com.google.android.ext.services"
  
- pm uninstall --user $usuario com.google.android.ext.shared  
- echo "desinstalando com.google.android.ext.shared"
+ pm disable-user --user $usuario com.google.android.ext.shared  
+ echo "Deshabilitando com.google.android.ext.shared"
  
- pm uninstall --user $usuario com.google.android.apps.wellbeing  
-echo "desinstalando com.google.android.apps.wellbeing"
+ pm disable-user --user $usuario com.google.android.apps.wellbeing  
+echo "Deshabilitando com.google.android.apps.wellbeing"
  
- pm uninstall --user $usuario com.google.android.calculator  
- echo "desinstalando com.google.android.calculator"
+ pm disable-user --user $usuario com.google.android.calculator  
+ echo "Deshabilitando com.google.android.calculator"
  
- pm uninstall --user $usuario com.google.android.calendar  
- echo "desinstalando com.google.android.calendar"
+ pm disable-user --user $usuario com.google.android.calendar  
+ echo "Deshabilitando com.google.android.calendar"
  
- pm uninstall --user $usuario com.google.android.ims  
- echo "desinstalando com.google.android.ims"
+ pm disable-user --user $usuario com.google.android.ims  
+ echo "Deshabilitando com.google.android.ims"
  
- pm uninstall --user $usuario com.google.android.configupdater  
- echo "desinstalando com.google.android.configupdater"
+ pm disable-user --user $usuario com.google.android.configupdater  
+ echo "Deshabilitando com.google.android.configupdater"
  
- pm uninstall --user $usuario com.google.android.apps.restore  
- echo "desinstalando com.google.android.apps.restore"
+ pm disable-user --user $usuario com.google.android.apps.restore  
+ echo "Deshabilitando com.google.android.apps.restore"
  
- pm uninstall --user $usuario com.google.android.setupwizard
- echo "desinstalando com.google.android.setupwizard"
+ pm disable-user --user $usuario com.google.android.setupwizard
+ echo "Deshabilitando com.google.android.setupwizard"
  
- pm uninstall --user $usuario com.google.android.apps.work.clouddpc  
-echo "desinstalando com.google.android.apps.work.clouddpc"
+ pm disable-user --user $usuario com.google.android.apps.work.clouddpc  
+echo "Deshabilitando com.google.android.apps.work.clouddpc"
 
-pm uninstall --user $usuario com.google.android.apps.docs  
- echo "desinstalando com.google.android.apps.docs"
+pm disable-user --user $usuario com.google.android.apps.docs  
+ echo "Deshabilitando com.google.android.apps.docs"
  
- pm uninstall --user $usuario com.google.android.apps.tachyon  
- echo "desinstalando com.google.android.apps.tachyon"
+ pm disable-user --user $usuario com.google.android.apps.tachyon  
+ echo "Deshabilitando com.google.android.apps.tachyon"
  
- pm uninstall --user $usuario com.google.android.apps.photos  
- echo "desinstalando com.google.android.apps.photos"
+ pm disable-user --user $usuario com.google.android.apps.photos  
+ echo "Deshabilitando com.google.android.apps.photos"
  
- pm uninstall --user $usuario com.google.android.gms  
- echo "desinstalando com.google.android.gms"
+ pm disable-user --user $usuario com.google.android.gms  
+ echo "Deshabilitando com.google.android.gms"
  
- pm uninstall --user $usuario com.google.android.onetimeinitializer  
- echo "desinstalando com.google.android.onetimeinitializer"
+ pm disable-user --user $usuario com.google.android.onetimeinitializer  
+ echo "Deshabilitando com.google.android.onetimeinitializer"
  
- pm uninstall --user $usuario com.google.android.music  
- echo "desinstalando com.google.android.music"
+ pm disable-user --user $usuario com.google.android.music  
+ echo "Deshabilitando com.google.android.music"
  
- pm uninstall --user $usuario com.google.android.partnersetup  
- echo  "desinstalando com.google.android.partnersetup "
+ pm disable-user --user $usuario com.google.android.partnersetup  
+ echo  "Deshabilitando com.google.android.partnersetup "
  
- pm uninstall --user $usuario com.google.android.apps.maps  
- echo "desinstalando com.google.android.apps.maps"
+ pm disable-user --user $usuario com.google.android.apps.maps  
+ echo "Deshabilitando com.google.android.apps.maps"
  
- pm uninstall --user $usuario com.google.android.apps.messaging  
- echo "desinstalando com.google.android.apps.messaging"
+ pm disable-user --user $usuario com.google.android.apps.messaging  
+ echo "Deshabilitando com.google.android.apps.messaging"
  
- pm uninstall --user $usuario com.google.android.printservice.recommendation
- echo "desinstalando com.google.android.printservice.recommendation"
+ pm disable-user --user $usuario com.google.android.printservice.recommendation
+ echo "Deshabilitando com.google.android.printservice.recommendation"
  
- pm uninstall --user $usuario com.google.android.syncadapters  
-echo "desinstalando com.google.android.syncadapters"
+ pm disable-user --user $usuario com.google.android.syncadapters  
+echo "Deshabilitando com.google.android.syncadapters"
 
- pm uninstall --user $usuario com.google.android.syncadapters
-echo "desinstalando com.google.android.syncadapters"
+ pm disable-user --user $usuario com.google.android.syncadapters
+echo "Deshabilitando com.google.android.syncadapters"
               
- pm uninstall --user $usuario com.google.android.syncadapters.contacts
- echo "desinstalando com.google.android.syncadapters.contacts"
+ pm disable-user --user $usuario com.google.android.syncadapters.contacts
+ echo "Deshabilitando com.google.android.syncadapters.contacts"
  
- pm uninstall --user $usuario com.google.android.gm
- echo "desinstalando com.google.android.gm"
+ pm disable-user --user $usuario com.google.android.gm
+ echo "Deshabilitando com.google.android.gm"
  
- pm uninstall --user $usuario com.google.android.inputmethod.latin
- echo "desinstalando teclado de google"
+ pm disable-user --user $usuario com.google.android.inputmethod.latin
+ echo "Deshabilitando teclado de google"
  
-pm uninstall --user $usuario com.google.android.youtube
-echo "desinstalando Youtube"
+pm disable-user --user $usuario com.google.android.youtube
+echo "Deshabilitando Youtube"
 
- pm uninstall --user $usuario com.google.android.marvin.talback
-echo "desinstalando com.google.android.marvin.talback"
+ pm disable-user --user $usuario com.google.android.marvin.talback
+echo "Deshabilitando com.google.android.marvin.talback"
 
- pm uninstall --user $usuario com.google.android.printservice.recommendation
-echo "desinstalando com.google.android.printservice.recommendation"
+ pm disable-user --user $usuario com.google.android.printservice.recommendation
+echo "Deshabilitando com.google.android.printservice.recommendation"
 
- pm uninstall --user $usuario com.google.android.feedback
- echo "desinstalando com.google.android.feedback"
+ pm disable-user --user $usuario com.google.android.feedback
+ echo "Deshabilitando com.google.android.feedback"
  
- pm uninstall --user $usuario com.google.android.gms.policy_sidecar_aps
- echo "desinstalando com.google.android.gms.policy_sidecar_aps"
-pm uninstall --user $usuario com.android.chrome
-echo "desinstalando chrome"
+ pm disable-user --user $usuario com.google.android.gms.policy_sidecar_aps
+ echo "Deshabilitando com.google.android.gms.policy_sidecar_aps"
+pm disable-user --user $usuario com.android.chrome
+echo "Deshabilitando chrome"
